a622db01f66c2cd50c89885f960cd8d8911951c5
Grégoire Aubertin <greg_abrn@icloud.com>
2025-05-28 15:24:16 +0200

    react-counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,50 @@
 import "./App.css";
+import { useState } from "react";
+
+const [counter, setCounter] = useState(0);
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  return (
+    <>
+      <header>
+        <img src="" alt="" />
+        <h1>React Counter</h1>
+      </header>
 
-  return <></>;
+      <main>
+        <button
+          onClick={() => {
+            setCounter(counter + 1);
+          }}
+        >
+          +
+        </button>
+
+        <button
+          onClick={() => {
+            setCounter((counter = 0));
+          }}
+        >
+          RESET
+        </button>
+
+        <button
+          onClick={() => {
+            setCounter(counter - 1);
+          }}
+        >
+          -
+        </button>
+      </main>
+
+      <footer>
+        <p>
+          Made with <span>React</span> at <span>Le Reacteur</span> by{" "}
+          <span>Greg</span>
+        </p>
+      </footer>
+    </>
+  );
 };
 
 export default App;
